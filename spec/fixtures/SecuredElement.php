d6f2e954baff70d986cb5a284d427a3529fcf4d2
Piotr <piotr.kapera@fsi.pl>
2015-06-12 16:07:24 +0200

    Password reset

--- spec/fixtures/SecuredElement.php
+++ spec/fixtures/SecuredElement.php
@@ -4,7 +4,7 @@ namespace FSi\Bundle\AdminSecurityBundle\spec\fixtures;
 
 use FSi\Bundle\AdminBundle\Admin\AbstractElement;
 use FSi\Bundle\AdminSecurityBundle\Admin\SecuredElementInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class SecuredElement extends AbstractElement implements SecuredElementInterface
@@ -25,7 +25,7 @@ class SecuredElement extends AbstractElement implements SecuredElementInterface
     {
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function setDefaultOptions(OptionsResolver $resolver)
     {
     }
 }
